3e1461e1bcb383e014bf45b92f11dd2ce6afb1fb
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-12 12:17:47 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -8,7 +8,7 @@ export default class Keypad extends Component {
   
   render(){
     return (
-      <input keyUp={this.handleKeyUp} />
+      <input type ="password" keyUp={this.handleKeyUp} />
       )
   }
 }
