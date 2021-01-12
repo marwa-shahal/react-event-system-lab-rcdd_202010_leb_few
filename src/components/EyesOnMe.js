411862db65e34fa7c70a5574a58f109278699dc1
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-12 12:15:37 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,2 +1,21 @@
 // Code EyesOnMe Component Here
-import React from 'react';
+import React, {Component} from 'react';
+
+
+export default class EyesOnMe extends Components {
+  
+  focus = () => {
+    
+  }
+  
+  blur = () => {
+    
+  }
+  
+  render(){
+    return (
+      <button focus ={this.focus}   blur= {this.blur} > </button>
+      )
+  }
+  
+}
