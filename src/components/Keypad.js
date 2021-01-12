0583221de11bc01b3cd5f3a126f9307c8deac393
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-12 12:08:03 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+import React from 'react';
+
+export default class Keypad extends Component {
+  constructor(){
+    super();
+    
+  }
+  
+  handleKeyUp = (value)=>{
+    console.log('Entering password....')
+  }
+  
+  render(){
+    return (
+      <input keyUp={handleKeyUp} />
+      )
+  }
+}
